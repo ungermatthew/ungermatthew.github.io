62a4fbfd2b98a93b1bc81ab390d58af4965fed08
DESKTOP-T479CH1\Unger <ungerandrew2@gmail.com>
2017-07-19 12:19:52 -0600

    Email added

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,3 @@
-function myMap() {
-    myCenter = new google.maps.LatLng(41.878114, -87.629798);
-    var mapOptions = {
-        center: myCenter,
-        zoom: 12, scrollwheel: false, draggable: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myCenter,
-    });
-    marker.setMap(map);
-}
 
 // Modal Image Gallery
 function onClick(element) {
